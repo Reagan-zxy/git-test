443f126a571c1135642eb3251f75f7762a544596
Reagan-zxy <80322953+Reagan-zxy@users.noreply.github.com>
2022-08-17 17:29:07 +0800

    pull test

--- src/main/java/com/zxy/git/GitTest.java
+++ src/main/java/com/zxy/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello hotfix test");
         System.out.println("hello git555555555");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
