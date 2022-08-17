444795b128ac36f518abfb5fc30eedacd7bdfa69
Reagan-zxy <1101541882@qq.com>
2022-08-17 16:54:02 +0800

    first commit

---
+++ src/main/java/com/zxy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zxy.git;
+
+/**
+ * @author zhangxiongyan
+ * @create 2022-08-17-16:47
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
