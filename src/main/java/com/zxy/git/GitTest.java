a42fc9d591ac7dc79f17695208fcfd34eafa24c9
Reagan-zxy <1101541882@qq.com>
2022-08-17 17:01:51 +0800

    hot-fix commit

--- src/main/java/com/zxy/git/GitTest.java
+++ src/main/java/com/zxy/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git222222");
         System.out.println("hello git3333333");
+        System.out.println("hello git4444444");
     }
 }
